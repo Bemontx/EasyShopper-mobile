5e097b650ce5c80fa73020773f04f91197197def
Jeisson Cardoso <101949185+Bemontx@users.noreply.github.com>
2026-02-04 14:07:50 -0600

    model interface product

---
+++ src/app/services/products.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from '../models/models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+  private apiUrl = 'https://localhost:7247/api/products';
+
+  constructor(private http: HttpClient) {}
+
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.apiUrl);
+  }
+}
