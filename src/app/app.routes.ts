1827464361738c1a448b45eab2ef3691a595f8a4
Jeisson Cardoso <101949185+Bemontx@users.noreply.github.com>
2026-02-04 15:55:19 -0600

    update icons/routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,54 +1,45 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-
   {
     path: '',
     redirectTo: 'login',
     pathMatch: 'full',
   },
-
   {
     path: 'login',
-    loadComponent: () =>
-      import('./pages/login/login.page').then(m => m.LoginPage)
+    loadComponent: () => import('./pages/login/login.page').then(m => m.LoginPage)
   },
-
   {
     path: 'register',
-    loadComponent: () =>
-      import('./pages/register/register.page').then(m => m.RegisterPage)
+    loadComponent: () => import('./pages/register/register.page').then(m => m.RegisterPage)
   },
-
   {
     path: 'tabs',
-    loadComponent: () =>
-      import('./pages/tabs/tabs.page').then(m => m.TabsPage)
+    loadComponent: () => import('./pages/tabs/tabs.page').then(m => m.TabsPage),
+    children: [
+      {
+        path: 'products',
+        loadComponent: () => import('./pages/products/products.page').then(m => m.ProductsPage)
+      },
+      {
+        path: 'cart',
+        loadComponent: () => import('./pages/cart/cart.page').then(m => m.CartPage)
+      },
+      {
+        path: '',
+        redirectTo: 'products',
+        pathMatch: 'full'
+      }
+    ]
   },
-
-  {
-    path: 'products',
-    loadComponent: () =>
-      import('./pages/products/products.page').then(m => m.ProductsPage)
-  },
-
-  {
-    path: 'cart',
-    loadComponent: () =>
-      import('./pages/cart/cart.page').then(m => m.CartPage)
-  },
-
+  
   {
     path: 'checkout',
-    loadComponent: () =>
-      import('./pages/checkout/checkout.page').then(m => m.CheckoutPage)
+    loadComponent: () => import('./pages/checkout/checkout.page').then(m => m.CheckoutPage)
   },
-
   {
     path: 'confirmation',
-    loadComponent: () =>
-      import('./pages/confirmation/confirmation.page')
-        .then(m => m.ConfirmationPage)
+    loadComponent: () => import('./pages/confirmation/confirmation.page').then(m => m.ConfirmationPage)
   }
-
 ];
--- src/app/pages/tabs/tabs.page.ts
+++ src/app/pages/tabs/tabs.page.ts
@@ -2,19 +2,40 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { RouterModule } from '@angular/router';
+import { 
+  IonTabs, 
+  IonTabBar, 
+  IonTabButton, 
+  IonIcon, 
+  IonLabel,
+  IonRouterOutlet
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons'; 
+import { pricetagOutline, cartOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: './tabs.page.html',
   styleUrls: ['./tabs.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    CommonModule, 
+    FormsModule, 
+    RouterModule,
+    IonTabs, 
+    IonTabBar, 
+    IonTabButton, 
+    IonIcon, 
+    IonLabel,
+    IonRouterOutlet
+  ]
 })
 export class TabsPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    addIcons({ pricetagOutline, cartOutline });
   }
 
+  ngOnInit() {}
 }
+
