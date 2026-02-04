d33140956f65a907f8012e4dbf4963cc4289e91d
Jeisson Cardoso <101949185+Bemontx@users.noreply.github.com>
2026-02-04 13:45:35 -0600

    config init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,36 +11,30 @@ export const routes: Routes = [
   {
     path: 'login',
     loadComponent: () =>
-      import('./pages/login/login.page')
-        .then(m => m.LoginPage)
+      import('./pages/login/login.page').then(m => m.LoginPage)
   },
 
   {
     path: 'register',
     loadComponent: () =>
-      import('./pages/register/register.page')
-        .then(m => m.RegisterPage)
+      import('./pages/register/register.page').then(m => m.RegisterPage)
   },
 
   {
     path: 'products',
     loadComponent: () =>
-      import('./pages/products/products.page')
-        .then(m => m.ProductsPage)
+      import('./pages/products/products.page').then(m => m.ProductsPage)
   },
 
   {
     path: 'cart',
     loadComponent: () =>
-      import('./pages/cart/cart.page')
-        .then(m => m.CartPage)
+      import('./pages/cart/cart.page').then(m => m.CartPage)
   },
 
   {
     path: 'checkout',
     loadComponent: () =>
-      import('./pages/checkout/checkout.page')
-        .then(m => m.CheckoutPage)
+      import('./pages/checkout/checkout.page').then(m => m.CheckoutPage)
   }
-
 ];
