44b456d2c948684482af8f8d3303eddca1d9a1e3
Jeisson Cardoso <101949185+Bemontx@users.noreply.github.com>
2026-02-05 11:39:40 -0600

    update IonicRoute

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,19 +1,24 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Router,RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { AuthService } from 'src/app/services/auth'; 
-import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
-
+import { 
+  IonContent, IonHeader, IonTitle, IonToolbar, 
+  IonInput, IonButton, IonItem, IonList 
+} from '@ionic/angular/standalone';
 
 @Component({
   standalone: true,
   selector: 'app-login',
   templateUrl: './login.page.html',
-  imports: [IonicModule, FormsModule, CommonModule,RouterModule]
+  imports: [
+    CommonModule, FormsModule, RouterModule,
+    IonContent, IonHeader, IonTitle, IonToolbar, 
+    IonInput, IonButton, IonItem, IonList 
+  ]
 })
 export class LoginPage {
-
   email = '';
   password = '';
 
@@ -22,24 +27,25 @@ export class LoginPage {
     private router: Router
   ) {}
 
-  login() {this.authService.login({
-    email: this.email,
-    password: this.password
-  }).subscribe({
-    next: (res: any) => {
-      const userId = res.id || res.Id; 
-      
-      if (userId) {
-        localStorage.setItem('userId', userId); 
-        localStorage.setItem('token', res.token || res.Token);
-        this.router.navigateByUrl('/tabs/products');
-      } else {
-        console.error('El backend no envió el ID del usuario', res);
+  login() {
+    this.authService.login({
+      email: this.email,
+      password: this.password
+    }).subscribe({
+      next: (res: any) => {
+        const userId = res.id || res.Id; 
+        if (userId) {
+          localStorage.setItem('userId', userId); 
+          localStorage.setItem('token', res.token || res.Token);
+          this.router.navigateByUrl('/tabs/products');
+        } else {
+          console.error('El backend no envió el ID del usuario', res);
+        }
+      },
+      error: (err) => {
+        alert('Credenciales incorrectas');
+        console.error(err);
       }
-    },
-    error: (err) => {
-      alert('Credenciales incorrectas');
-    }
     });
   }
 }
